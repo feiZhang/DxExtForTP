179d3d20eb04390fca9512e9949bc9f2bd3573f9
hanmeiyan <hmy2009@126.com>
2013-04-17 15:21:49 +0800

    角色表

--- DxExtRoleModel.class.php
+++ DxExtRoleModel.class.php
@@ -1,6 +1,11 @@
 <?php
 class DxExtRoleModel extends DxExtCommonModel {
-    protected $listFields = array(
+    public $listFields = array(
+            'role_id'    =>array('type'=>'int','size'=>10 ,'default' =>'0'),
+            'name'       =>array('type'=>'varchar','size'=>45 ,'title'=>'角色名'),
+            'menu_ids'   =>array('type'=>'varchar','size'=>1000,'title'=>'菜单ID'),
+            'shortcut_ids'=>array('type'=>'varchar','size'=>1000,'title'=> '快捷方式ids'),
+            'desk_ids'   =>array('type'=>'varchar','size'=>1000,'title'=>'桌面菜单ids'),
     );
     
     protected $modelInfo=array(
