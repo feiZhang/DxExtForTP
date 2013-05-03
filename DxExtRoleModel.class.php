3fd69241f718e434446d6ccedbdcf82e7174b3e9
hanmeiyan <hmy2009@126.com>
2013-05-03 18:48:18 +0800

    增加得到菜单ID 的功能

--- DxExtRoleModel.class.php
+++ DxExtRoleModel.class.php
@@ -11,5 +11,11 @@ class DxExtRoleModel extends DxExtCommonModel {
     protected $modelInfo=array(
         "title"=>'用户角色','readOnly'=>true,"dictTable"=>"name","helpInfo"=>"用户角色由系统初始化，无法进行增删!"
     );
+    public function getMenuID(){
+        $role_id			= session("role_id");
+        if(intval($role_id)<1) return "0";
+        $data	= $this->where(array("role_id"=>$role_id))->field("menu_ids")->find();
+        return empty($data["menu_ids"])?"0":$data["menu_ids"];
+    }
 }
 ?>
