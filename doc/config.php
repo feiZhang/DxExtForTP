a0b8643de4b669530cb3cb71f3771126abff005a
U-fei-win7\fei <fei@fei-win7.(none)>
2013-07-02 14:29:19 +0800

    增加框架安装文档

---
+++ doc/config.php
@@ -0,0 +1,24 @@
+<?php
+if (! defined ( 'THINK_PATH' ))
+    exit ();
+
+$theProjectConfig = require (DXINFO_PATH . "/config.inc.php");
+$theAppConfig = array (
+	//项目应用配置
+);
+
+$theProjectDatabase = require ("./database.inc.php");
+
+if (file_exists ( "./database.php" ))
+    $theAppDatabase = require ("database.php");
+else 
+    $thieAppDatabase = array();
+
+if (file_exists ( "./debug.inc.php" ))
+    $theDebugConfig = require ("./debug.inc.php");
+else
+    $theDebugConfig = array ();
+
+$endConfig = array_merge ( $theProjectConfig, $theProjectDatabase, $theAppDatabase, $theAppConfig, $theDebugConfig );
+// var_dump($endConfig)
+return $endConfig;
